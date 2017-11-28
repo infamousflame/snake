ec35af2371284f41c262e9896ef639ee37a738bd
Owen <owen@agrian.com>
2017-11-28 12:29:20 -0800

    tail semi working

--- game.js
+++ game.js
@@ -13,22 +13,25 @@ window.onresize = resize
 resize()
 
 
-var state = {
-  x: (width / 2),
-  y: (height / 2),
-  tail: 1
-}
+var state = [
+  {
+    x: (width / 2),
+    y: (height / 2),
+  }
+]
+
+
 
 spawnApple()
 
 
 function draw() {
   ctx.clearRect(0, 0, width, height)
-  ctx.font = "30px Arial";
-  ctx.fillText("Ayy your school lmao: " + state.tail, 100, 100);
+  ctx.font = "60px Arial";
+  ctx.fillText("Ayy your score lmao: " + state.length, 100, 100);
   ctx.fillStyle = 'lightgreen';
-  for(var i = 0; i < state.tail; i++){
-    ctx.fillRect(state.x, state.y - (30 * i), 30, 30);
+  for(var i = 0; i < state.length; i++){
+    ctx.fillRect(state[i].x, state[i].y, 30, 30);
   }
   ctx.fillStyle = 'red';
   ctx.fillRect(state.appleX - 30, state.appleY - 30, 30, 30);
@@ -54,64 +57,86 @@ function keyDown(event){
 function keyUp(event){
   state.keyPressed = event.key;
 }
-var offset = 2;
+var offset = 3;
 
 function update(progress) {
 
-  if((state.x >= state.appleX - 30 && state.x <= state.appleX + 40) && (state.y <= state.appleY + 40 && state.y >= state.appleY - 30)){
+  if((state[0].x >= state.appleX - 30 && state[0].x <= state.appleX + 40) && (state[0].y <= state.appleY + 40 && state[0].y >= state.appleY - 30)){
       spawnApple();
-      state.tail++;
+      state.push({});
   }
 
-  // console.log("X: " + state.x + " Y: " + state.y);
+  // console.log("X: " + state[0].x + " Y: " + state[0].y);
   // console.log("Apple X: " + state.appleX + " Apple Y: " + state.appleY);
 
 
 
   switch(state.direction){
     case "right":
-      state.x += progress/offset;
-      if(state.x > width){
-        state.x = 0;
+      state[0].x += progress/offset;
+      if(state[0].x > width){
+        state[0].x = 0;
       }
       break;
     case "left":
-      state.x -= progress/offset;
-      if(state.x <= 0){
-        state.x = width;
+      state[0].x -= progress/offset;
+      if(state[0].x <= 0){
+        state[0].x = width;
       }
       break;
     case "up":
-      state.y -= progress/offset;
-      if(state.y <= 0){
-        state.y = height;
+      state[0].y -= progress/offset;
+      if(state[0].y <= 0){
+        state[0].y = height;
       }
       break;
     case "down":
-      state.y += progress/offset;
-      if(state.y > height){
-        state.y = 0;
+      state[0].y += progress/offset;
+      if(state[0].y > height){
+        state[0].y = 0;
       }
       break;
   }
 }
 
-function loop(timestamp) {
-  var progress = (timestamp - lastRender)
+var fps = 15;
+var now;
+var then = Date.now();
+var interval = 1000/fps;
+var delta;
 
-  update(progress)
-  draw()
 
-  lastRender = timestamp
-  window.requestAnimationFrame(loop)
+function snakeTail(firstState){
+  for(var i = 0; i < state.length; i++){
+    if(i != 0){
+      state[i].x = firstState[i - 1].x;
+      state[i].y = firstState[i - 1].y;
+      // console.log(state[i].x);
+      console.log(i);
+    }
+  }
+}
+var idk = 0;
+
+function loop() {
+    requestAnimationFrame(loop);
+    now = Date.now();
+    delta = now - then;
+    // if(state.length > 1){
+      // console.log(state[0].x + " " + state[1].x);
+    // }
+    if (delta > interval) {
+        update(delta)
+        draw()
+        snakeTail(state);
+        then = now - (delta % interval);
+    }
 }
 
 function spawnApple(){
   state.appleX = Math.floor((Math.random() * width) + 1);
   state.appleY = Math.floor((Math.random() * height) + 1);
 }
-
-
 
 var lastRender = 0
 window.requestAnimationFrame(loop);
